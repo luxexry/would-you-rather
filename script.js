17f2bba2d0c7f451aa9bef207dbead13ace0d014
luxexry x <luxexry@gmail.com>
2023-11-17 02:39:31 +0000

    m

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var d = 1;
+var VideoState = 1;
 var x = 0;
 var i = 0;
+var VideoIDState = 1;
 var audioElement = document.getElementById("audioElement");
 
 var statements = []; // Store the statements from the JSON file
@@ -11,6 +13,11 @@ function playNextAudio() {
     var nextStatement = statements.shift(); // Get and remove the first statement from the array
     PlayAudio("Would you rather " + nextStatement.statement);
   }
+}
+
+function playByID(id) {
+  var nextStatement = statements[VideoIDState];
+  PlayAudio("Would you rather " + nextStatement.statement);
 }
 
 function toggleVoteOverlay() {
@@ -26,39 +33,55 @@ function VideoCompliation() {
     if (event.key === "d") {
       toggleVoteOverlay();
       // Check if the element exists
+      var option1 = document.getElementById("option1");
+      var option2 = document.getElementById("option2");
+      var or_text = document.getElementById("or-text");
+      
+      setInterval(function () {
+        if (option1 && option2) {
+          // Change the id of the element to the random number
+          if (VideoState === 1) {
+            VideoState = 2;
+            applyAnimation('image1', 'animate');
+            applyAnimation('image2', 'animate');
+  
+            or_text.style.fontSize = "x-large";
+            or_text.innerHTML = "⌛";
+            applyAnimation('or-text', 'rotatetheOR');
+  
+            playByID(VideoIDState);
+  
+            // Add an "ended" event listener to the text-to-speech audio
+            audioElement.addEventListener("ended", function () {
+              // When the text-to-speech audio has finished playing, play the clock audio
+              PlayUtilityAudio("clock.mp3");
+              wait(2000);
+              toggleVoteOverlay();
+            });
+          } else if (VideoState === 2) {
+            VideoState = 1;
+            
+            toggleVoteOverlay();
+            removeAnimation('image1', 'animate');
+            removeAnimation('image2', 'animate');
+  
+            or_text.style.fontSize = "medium";
+            or_text.innerHTML = "OR";
+            removeAnimation('or-text', 'rotatetheOR');
 
-      if (element) {
-        // Change the id of the element to the random number
-        if (d === 1) {
-          d = 2;
-          applyAnimation('image1', 'animate');
+            // Perform actions using the current id
+            populateHTMLWithJSON(VideoIDState);
+            updateVoteOverlay(VideoIDState);
 
-          or_text.style.fontSize = "x-large";
-          or_text.innerHTML = "⌛";
-          applyAnimation('or-text', 'rotatetheOR');
-
-          playNextAudio();
-
-          // Add an "ended" event listener to the text-to-speech audio
-          audioElement.addEventListener("ended", function () {
-            // When the text-to-speech audio has finished playing, play the clock audio
-            PlayUtilityAudio("clock.mp3");
-            wait(2000);
-          });
-        } else if (d === 2) {
-          d = 1;
-          removeAnimation('image1', 'animate');
-
-          or_text.style.fontSize = "medium";
-          or_text.innerHTML = "OR";
-          removeAnimation('line', 'colorLine');
-          removeAnimation('or-text', 'rotatetheOR');
-
-          StopAudio();
+            // Increment id and use modulo to cycle back to 1 if needed
+            VideoIDState = (VideoIDState % statements.length) + 1;
+  
+            StopAudio();
+          }
+        } else {
+          console.log('Element with id "option2" does not exist');
         }
-      } else {
-        console.log('Element with id "option2" does not exist');
-      }
+      }, 5000);
     }
   });
 }
@@ -260,8 +283,6 @@ function updateVoteOverlay(id) {
 }
 // Call the function to populate the HTML with JSON data
 var id = 1;
-populateHTMLWithJSON(id);
-updateVoteOverlay(id);
 
 // wait function
 function wait(ms) {
